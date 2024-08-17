87854930de1bba793d01363011d161d44241b76b
axeloon <axelquezadac@gmail.com>
2024-08-17 18:45:22 -0400

    feat: .- Se crean eventos de comienzo y fin de la API .-se crea un endpoint de prueba para el scrapper.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from .config import X_API_KEY, X_API_SECRET, X_BEARER_TOKEN
 from .scrapper.x_scrapper import XScraper
 import logging
 
@@ -8,24 +7,24 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-
 x_scraper = XScraper()
 
+@app.on_event("startup")
+async def startup_event():
+    await x_scraper.start()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await x_scraper.close()
+
 @app.get("/api/search_user/{username}")
-def search_user(username: str):
+async def search_user(username: str):
     logger.info(f"Buscando usuario: {username}")
     try:
-        user_data = x_scraper.search_user(username)
+        html_user_data = await x_scraper.fetch_user_profile_html(username)
+        user_data = x_scraper.process_user_html(html_user_data)
         logger.info(f"Usuario encontrado: {user_data}")
         return user_data
     except Exception as e:
         logger.error(f"Error al buscar usuario: {e}")
-        raise HTTPException(status_code=500, detail="Error al buscar usuario")
-
-
-@app.get("/api/hola_mundo")
-def hola_mundo():
-    logger.info(f"X_API_KEY: {X_API_KEY}")
-    logger.info(f"X_API_SECRET: {X_API_SECRET}")
-    logger.info(f"X_BEARER_TOKEN: {X_BEARER_TOKEN}")
-    return {"message": "Hola mundo"}
+        raise HTTPException(status_code=500, detail=str(e))
