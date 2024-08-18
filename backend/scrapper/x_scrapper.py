671e0ffa99f90f922d41746ea8c985690c6ed86c
axeloon <axelquezadac@gmail.com>
2024-08-18 16:07:10 -0400

    feat: Se crea un método para obtener tweets, por ahora solo obtenemos el primero

--- backend/scrapper/x_scrapper.py
+++ backend/scrapper/x_scrapper.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import re
 
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
@@ -19,7 +20,7 @@ class XScraper:
 
     async def start(self):
         self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.chromium.launch(headless=False)
+        self.browser = await self.playwright.chromium.launch(headless=True)
         self.context = await self.browser.new_context(
                 viewport={"width": 1920, "height": 1080},
                 user_agent=USER_AGENT
@@ -36,22 +37,17 @@ class XScraper:
         logger.info(f"Sesión Iniciada en X con el email {X_EMAIL}")
 
 
-    async def get_tweets(self, username, max_results=10):
-        url = f"https://twitter.com/{username}"
-        await self.page.goto(url)
-        # Implementa la lógica para extraer tweets aquí
-        tweets = await self.page.query_selector_all("article")
-        return tweets[:max_results]
-
-
     async def fetch_user_profile_html(self, username):
         await self.page.goto(f"https://x.com/{username}")
         await asyncio.sleep(2)
         # Esperar a que el contenido relevante esté cargado
-        await self.page.wait_for_selector('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div', state='attached')
+        await self.page.wait_for_selector('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div', state='attached')
         # Obtener el HTML de la sección deseada
+        await asyncio.sleep(2)
         content_html = await self.page.inner_html('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div')
-        return content_html
+        tweets_html = await self.page.inner_html('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div')
+        return content_html, tweets_html
+
 
     def process_user_html(self, html_content):
         soup = BeautifulSoup(html_content, 'html.parser')
@@ -60,6 +56,10 @@ class XScraper:
         verified_icon = soup.find('svg', {'aria-label': 'Verified account'})
         verified = verified_icon is not None
         
+        # Buscar el nombre del usuario
+        user_name_element = soup.find('div', {'class': 'css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-37j5jr r-adyw6z r-135wba7 r-1vr29t4 r-1awozwy r-6koalj r-1udh08x'})
+        user_name = user_name_element.text if user_name_element else None
+
         # Buscar la fecha de unión
         join_date = soup.find('span', text=lambda text: text and 'Joined' in text)
         join_date = join_date.text if join_date else None
@@ -74,12 +74,27 @@ class XScraper:
                 value = numbers[0].text
                 label = labels[i]
                 followers_info[label] = value
-
         return {
+            "user_name": user_name,
             "verified": verified,
             "join_date": join_date,
             "followers_info": followers_info
         }
+    
+
+    def extract_tweets(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # Usar una expresión regular para encontrar la sección de los tweets
+        tweets_section = soup.find('div', {'class': 'css-175oi2r'})
+        # Extraer los primeros 5 tweets
+        tweets = []
+        if tweets_section:
+        # Buscar cada tweet por su etiqueta específica y clase
+            tweet_elements = tweets_section.find_all('div', {'class': 'css-175oi2r r-1igl3o0 r-qklmqi r-1adg3ll r-1ny4l3l'}, limit=5)
+            for tweet in tweet_elements:
+                tweet_text = tweet.get_text(strip=True)  # Extrae el texto de cada tweet
+                tweets.append(tweet_text)        
+        return tweets
 
 
     async def close(self):
