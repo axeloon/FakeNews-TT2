2da35cde576c5357a3ee2adf96199cfd475d74cc
axeloon <axelquezadac@gmail.com>
2024-08-27 01:49:50 -0400

    feat: Se crea un delimitador de la cantidad de tweets a extraer

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from .scrapper.x_scrapper import XScraper
 import logging
 
@@ -18,12 +18,12 @@ async def shutdown_event():
     await x_scraper.close()
 
 @app.get("/api/search_user/{username}")
-async def search_user(username: str):
-    logger.info(f"Buscando usuario: {username}")
+async def search_user(username: str, tweets_len: int = Query(30, description="Cantidad de tweets a procesar")):
+    logger.info(f"Buscando usuario: {username}, para encontrar los {tweets_len} primeros tweets")
     try:
         html_user_data, tweets_html = await x_scraper.fetch_user_profile_html(username)
         user_data = x_scraper.process_user_html(html_user_data)
-        tweets = x_scraper.extract_tweets(tweets_html)
+        tweets = x_scraper.extract_tweets(tweets_html, tweet_limit=tweets_len)
         logger.info(f"Usuario encontrado: {user_data}, Tweets: {len(tweets)}")
         return {
             "user_data": user_data,
