82c356ac9d5052328ba9d1f58109bc70e7b99ab4
axeloon <axelquezadac@gmail.com>
2024-09-21 18:19:56 -0300

    feat: se integra el almacenamiento de los tweets al flujo actual

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, HTTPException, Query, BackgroundTasks
+from fastapi import FastAPI, HTTPException, Query, BackgroundTasks, Depends
+from sqlalchemy.orm import Session
 from .scrapper.XScrapper import XScrapper
 from .services.FileService import FileService
 from .constant import X_TWEET_IMAGE_PATH
-from .models import SearchUserResponse
+from .utils.response_models import SearchUserResponse
+from .database.connection import get_db
+from .database.use_cases import NoticiaRedesSocialesUseCase
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -21,12 +24,16 @@ async def shutdown_event():
     await x_scrapper.close()
 
 @app.get("/api/search_user/{username}", response_model=SearchUserResponse)
-async def search_user(background_tasks: BackgroundTasks, 
-                      username: str, 
-                      tweets_len: int = Query(30, description="Cantidad de tweets a procesar")):
+async def search_user(
+    background_tasks: BackgroundTasks, 
+    username: str, 
+    tweets_len: int = Query(30, description="Cantidad de tweets a procesar"),
+    db: Session = Depends(get_db)
+):
     logger.info(f"Buscando usuario: {username}, para encontrar los {tweets_len} primeros tweets")
     try:
         result = await x_scrapper.search_user(username, tweet_limit=tweets_len)
+        background_tasks.add_task(NoticiaRedesSocialesUseCase.store_search_result, db, result)
         background_tasks.add_task(FileService.clean_folders, [X_TWEET_IMAGE_PATH])
         return result
     except Exception as e:
