50dea6d6547d77e4398145279a5dc62fa221f089
axeloon <axelquezadac@gmail.com>
2024-09-21 18:17:38 -0300

    fix: correcciones de logs

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -6,7 +6,7 @@ from ..config import DATABASE_URL
 
 logger = logging.getLogger(__name__)
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -14,13 +14,13 @@ Base = declarative_base()
 def get_db():
     db = SessionLocal()
     try:
-        logger.info(f"Creando nueva sesión de base de datos {db}")
+        logger.info("Creando nueva sesión de base de datos")
         yield db
     except Exception as e:
         logger.error(f"Error al crear sesión de base de datos: {e}")
         raise
     finally:
-        logger.info(f"Cerrando sesión de base de datos {db}")
+        logger.info("Cerrando sesión de base de datos")
         try:
             db.close()
         except Exception as e:
