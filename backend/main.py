6d104ce9807965f8d0dd53ea3babc301543e03fb
axeloon <axelquezadac@gmail.com>
2024-09-21 16:08:20 -0300

    feat: se modifica la respuesta del método para dejar mas limpio el main

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, HTTPException, Query
-from .scrapper.x_scrapper import XScraper
+from fastapi import FastAPI, HTTPException, Query, BackgroundTasks
+from .scrapper.x_scrapper import XScrapper
+from .services.FileService import FileService
+from .constant import X_TWEET_IMAGE_PATH
+from .models import SearchUserResponse
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -7,28 +10,25 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-x_scraper = XScraper()
+x_scrapper = XScrapper()
 
 @app.on_event("startup")
 async def startup_event():
-    await x_scraper.start()
+    await x_scrapper.start()
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    await x_scraper.close()
+    await x_scrapper.close()
 
-@app.get("/api/search_user/{username}")
-async def search_user(username: str, tweets_len: int = Query(30, description="Cantidad de tweets a procesar")):
+@app.get("/api/search_user/{username}", response_model=SearchUserResponse)
+async def search_user(background_tasks: BackgroundTasks, 
+                      username: str, 
+                      tweets_len: int = Query(30, description="Cantidad de tweets a procesar")):
     logger.info(f"Buscando usuario: {username}, para encontrar los {tweets_len} primeros tweets")
     try:
-        html_user_data, tweets_html = await x_scraper.fetch_user_profile_html(username)
-        user_data = x_scraper.process_user_html(html_user_data)
-        tweets = await x_scraper.extract_tweets(tweets_html, tweet_limit=tweets_len)
-        logger.info(f"Usuario encontrado: {user_data}, Tweets: {len(tweets)}")
-        return {
-            "user_data": user_data,
-            "tweets": tweets
-        }
+        result = await x_scrapper.search_user(username, tweet_limit=tweets_len)
+        background_tasks.add_task(FileService.clean_folders, [X_TWEET_IMAGE_PATH])
+        return result
     except Exception as e:
         logger.error(f"Error al buscar usuario: {e}")
         raise HTTPException(status_code=500, detail=str(e))
