be16f1b440549d16c824ce1035169993df1bcc56
axeloon <axelquezadac@gmail.com>
2024-10-20 14:47:46 -0300

    feat: se actualizan el modelo de BBDD para agregar una nueva tabla is_fake (bool) y todas las tables quedan en nullable=True

--- backend/database/db_models.py
+++ backend/database/db_models.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import Column, Integer, String, Date, DateTime, Text
+from sqlalchemy import Column, Integer, String, Date, DateTime, Text, Boolean
 
 from .connection import Base
 
@@ -7,10 +7,11 @@ class NoticiaRedesSociales(Base):
     __tablename__ = "noticias_redes_sociales"
 
     id = Column(Integer, primary_key=True, index=True)
-    source = Column(String(50))
-    title = Column(Text)
-    content = Column(Text)
-    publication_date = Column(Date)
-    author = Column(String(100))
+    source = Column(String(50), nullable=True)
+    title = Column(Text, nullable=True)
+    content = Column(Text, nullable=True)
+    publication_date = Column(Date, nullable=True)
+    author = Column(String(100), nullable=True)
+    is_false = Column(Boolean, nullable=True, default=True)
     created_at = Column(DateTime, nullable=True, default=datetime.datetime.utcnow)
     updated_at = Column(DateTime, nullable=True, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
